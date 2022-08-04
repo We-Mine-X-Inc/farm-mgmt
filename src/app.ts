54bb1e514f7e14d75b0f4ad89df589c54e817d4c
adwemine <108937310+adwemine@users.noreply.github.com>
2022-08-04 19:02:32 -0400

    Current Progress containing most of the production code.

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,18 @@
-import compression from 'compression';
-import express from 'express';
-import morgan from 'morgan';
-import { connect, set } from 'mongoose';
-import swaggerJSDoc from 'swagger-jsdoc';
-import swaggerUi from 'swagger-ui-express';
-import { NODE_ENV, PORT, LOG_FORMAT } from '@config';
-import { dbConnection } from '@databases';
-import { Routes } from '@interfaces/routes.interface';
-import errorMiddleware from '@middlewares/error.middleware';
-import { logger, stream } from '@utils/logger';
+import compression from "compression";
+import express from "express";
+import morgan from "morgan";
+import { connect, set } from "mongoose";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import { NODE_ENV, PORT, LOG_FORMAT } from "@config";
+import { dbConnection } from "@databases";
+import { Routes } from "@interfaces/routes.interface";
+import errorMiddleware from "@middlewares/error.middleware";
+import { logger, stream } from "@utils/logger";
+import Agenda from "agenda";
+import { switchGoldshellPool } from "@poolswitch/goldshell-switcher";
+import { switchAntminerPool } from "./poolswitch/antminer-switcher";
+import poolSwitchScheduler from "./scheduler/pool-switch-scheduler";
 
 class App {
   public app: express.Application;
@@ -17,10 +21,11 @@ class App {
 
   constructor(routes: Routes[]) {
     this.app = express();
-    this.env = NODE_ENV || 'development';
+    this.env = NODE_ENV || "development";
     this.port = PORT || 3000;
 
     this.connectToDatabase();
+    this.initiliazeSchedulers();
     this.initializeMiddlewares();
     this.initializeRoutes(routes);
     this.initializeSwagger();
@@ -41,11 +46,45 @@ class App {
   }
 
   private connectToDatabase() {
-    if (this.env !== 'production') {
-      set('debug', true);
+    if (this.env !== "production") {
+      set("debug", true);
     }
 
     connect(dbConnection.url, dbConnection.options);
+  }
+
+  private async initiliazeSchedulers() {
+    poolSwitchScheduler.startNewJobs([
+      {
+        minerId: "",
+      },
+    ]);
+    // switchPool({
+    //   ipAddress: "192.168.88.165",
+    //   toClientPool: true,
+    //   clientPool: {
+    //     url: "stratum+tcp://kda.ss.poolmars.net:5200",
+    //     user: "k:0907e28f36919517964993dfbb33fb53d421b0acc41d1c070a41ca0f84083c4b+pps.kdlite_1",
+    //   },
+    //   // clientPool: {
+    //   //   url: "stratum+tcp://kda.ss.poolmars.net:5200",
+    //   //   user: "k:03148fcea3fe47cb800fa66e869795fbaaa402c5f0e4437eb42ddbb613276be1+pps.kd2",
+    //   // },
+    // companyPool: {
+    //   url: "stratum+tcp://kda.ss.poolmars.net:5200",
+    //   user: "k:fd93de931359a2f15ba2aacdd9525e3783af0e975fe39195ba9f4cf6abeb8ff3+pps.kd6SE_1",
+    // },
+    //   macAddress: "",
+    // });
+    switchAntminerPool({
+      ipAddress: "192.168.88.72",
+      toClientPool: true,
+      clientPool: {
+        url: "stratum+tcp://us-east.stratum.slushpool.com:3333",
+        user: "tywright",
+      },
+      macAddress: "3C:A3:08:74:81:87",
+    });
   }
 
   private initializeMiddlewares() {
@@ -56,8 +95,8 @@ class App {
   }
 
   private initializeRoutes(routes: Routes[]) {
-    routes.forEach(route => {
-      this.app.use('/', route.router);
+    routes.forEach((route) => {
+      this.app.use("/", route.router);
     });
   }
 
@@ -65,16 +104,16 @@ class App {
     const options = {
       swaggerDefinition: {
         info: {
-          title: 'REST API',
-          version: '1.0.0',
-          description: 'Internal API - Manages Users and Pool Switching',
+          title: "REST API",
+          version: "1.0.0",
+          description: "Internal API - Manages Users and Pool Switching",
         },
       },
-      apis: ['swagger.yaml'],
+      apis: ["swagger.yaml"],
     };
 
     const specs = swaggerJSDoc(options);
-    this.app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+    this.app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
   }
 
   private initializeErrorHandling() {
