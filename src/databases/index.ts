3d5984e538867dbb7744652a7ad681c440333c30
adwemine <108937310+adwemine@users.noreply.github.com>
2022-10-15 13:30:56 -0400

    update configs for production quality

--- src/databases/index.ts
+++ src/databases/index.ts
@@ -1,13 +1,5 @@
-import {
-  DB_HOST,
-  DB_PORT,
-  DB_DATABASE,
-  DB_IS_SRV,
-  DB_PASSWORD,
-  DB_CLUSTER,
-} from "@config";
+import { DB_HOST, DB_DATABASE, DB_PASSWORD, DB_CLUSTER } from "@config";
 
 export const dbConnection = {
-  // url: `mongodb${DB_IS_SRV ? '+srv' : ''}://${DB_HOST}:${DB_PORT? DB_PORT:DB_PASSWORD}${DB_CLUSTER ? '@' + DB_CLUSTER:''}/${DB_DATABASE}`,
-  url: `mongodb+srv://dev:eZ0.hQ5%2BhJ8_aE8%40@cluster0.j6vfjac.mongodb.net/${DB_DATABASE}`,
+  url: `mongodb+srv://${DB_HOST}:${DB_PASSWORD}@${DB_CLUSTER}/${DB_DATABASE}`,
 };
