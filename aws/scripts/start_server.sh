88cfc72b4c2983cb3adf2e94ee58caf7ca821dff
adwemine <108937310+adwemine@users.noreply.github.com>
2022-11-15 00:44:30 -0500

    Add nvm PATH and DIR

--- aws/scripts/start_server.sh
+++ aws/scripts/start_server.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 cd /home/ubuntu/applications/farm-mgmt
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 $(dirname $(nvm which current))/npm run deploy:$NODE_ENV
