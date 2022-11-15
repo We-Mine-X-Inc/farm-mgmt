d18bfdc8107906d84398fdb0fb213804eb4fd792
adwemine <108937310+adwemine@users.noreply.github.com>
2022-11-15 00:51:09 -0500

    Try eval of ruby env so that other commands work

--- aws/scripts/start_server.sh
+++ aws/scripts/start_server.sh
@@ -4,5 +4,6 @@ cd /home/ubuntu/applications/farm-mgmt
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+eval "$(rbenv init -)"
 
 $(dirname $(nvm which current))/npm run deploy:$NODE_ENV
