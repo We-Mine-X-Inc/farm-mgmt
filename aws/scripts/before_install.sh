3d706572dbd481ab554e38e18b6dc2528b87858f
adwemine <108937310+adwemine@users.noreply.github.com>
2022-11-15 01:58:18 -0500

    Update before_install.sh

--- aws/scripts/before_install.sh
+++ aws/scripts/before_install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-export HOME="/home/ubuntu"
-
 # Delete the old  directory as needed.
 if [ -d /home/ubuntu/applications/farm-mgmt ]; then
     rm -rf /home/ubuntu/applications/farm-mgmt
