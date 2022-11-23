56a68ad608da5f9bc76394bc889c2820a187e878
adwemine <108937310+adwemine@users.noreply.github.com>
2022-11-23 03:29:56 -0500

    Retry dev deployment

--- aws/scripts/start_server.sh
+++ aws/scripts/start_server.sh
@@ -5,5 +5,6 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+sudo -s
 npm install
 npm run deploy:$NODE_ENV
